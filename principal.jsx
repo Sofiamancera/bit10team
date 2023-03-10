05a563cb81bcc3d7269b09b56b3325bdfb74a11e
Sofia Mancera <118775718+Sofiamancera@users.noreply.github.com>
2023-03-09 21:01:03 -0600

    Create principal.jsx

---
+++ principal.jsx
@@ -0,0 +1,10 @@
+importar  reaccionar  desde  'reaccionar'
+importar  ReactDOM  desde  'react-dom/client'
+Importar  aplicación  desde  './Aplicación'
+importar  './index.css'
+
+ReactDOM . createRoot ( documento . getElementById ( 'raíz' ) ) . hacer (
+  < Reaccionar . Modo estricto >
+    < Aplicación  / >
+  < / Reaccionar . Modo estricto > ,
+)
